68fcab1ad0d24e4e1bf7c1718e358fe69608c167
Zakria0 <69174113+Zakria0@users.noreply.github.com>
2026-09-25 14:14:00 +0000

    Update: Build a Tree for a Random Forest

--- labs/0026-build-a-tree-for-a-random-forest/solution.py
+++ labs/0026-build-a-tree-for-a-random-forest/solution.py
@@ -20,7 +20,7 @@ class DecisionTree:
     """
 
     def __init__(self, max_depth=None, min_samples_split=2,
-                 max_features=16, splitter='random', random_state=None):
+                 max_features=16, splitter='fully_random', random_state=None):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_features = max_features      
